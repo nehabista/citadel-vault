efcd6bce6f7fbf8144860c8e6922ed9515c4ffa9
Keshang Lama <Keshanglama456@gmail.com>
2026-03-03 10:00:00 +1100

    CIT-65: feat: build password generator with entropy visualization

--- lib/features/password_generator/presentation/pages/generator_page.dart
+++ lib/features/password_generator/presentation/pages/generator_page.dart
@@ -1,3 +1,4 @@
+// P2: Enhanced password generator with entropy visualization
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
